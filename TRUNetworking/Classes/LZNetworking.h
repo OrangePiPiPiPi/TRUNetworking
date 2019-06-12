9dba5baef354ebdc0bb17ec5c4a826c34d94ba63
fankai <fank@izkml.com>
2019-06-12 10:15:00 +0800

    init demo

---
+++ TRUNetworking/Classes/LZNetworking.h
@@ -0,0 +1,16 @@
+//
+//  LZNetworking.h
+//  FBSnapshotTestCase
+//
+//  Created by fank on 2019/6/12.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LZNetworking : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TRUNetworking/Classes/LZNetworking.m
@@ -0,0 +1,20 @@
+//
+//  LZNetworking.m
+//  FBSnapshotTestCase
+//
+//  Created by fank on 2019/6/12.
+//
+
+#import "LZNetworking.h"
+
+@implementation LZNetworking
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
